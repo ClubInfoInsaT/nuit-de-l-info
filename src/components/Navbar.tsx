97b74cb91e7958915ca0b48e7e239209eee3390d
Gérald LEBAN <gerald.leban0@gmail.com>
2022-12-01 22:10:44 +0100

    add top navigation bar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Bars3Icon } from "./Icons";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between">
+      <a href="/" className="text-xl font-bold">
+        {/* Brand Name */}
+        Lorem Ipsum
+      </a>
+      {/* Actions buttons */}
+      <div className="hidden gap-2 md:flex">
+        <button className="rounded-lg border-2 border-gray-900 px-3 py-2 font-bold hover:bg-gray-200">
+          Action 1
+        </button>
+        <button className="rounded-lg border-2 border-gray-900 bg-gray-900 px-3 py-2 font-bold text-white hover:border-gray-700 hover:bg-gray-700">
+          Action 2
+        </button>
+      </div>
+
+      {/* Hamburger Menu */}
+      <button className="rounded-lg p-2 hover:bg-gray-200 md:hidden">
+        <Bars3Icon strokeWidth={2} />
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
