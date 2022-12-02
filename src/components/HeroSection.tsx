17da2ade9327c892b77f164ec6a77a44feb1ad39
Gérald LEBAN <gerald.leban0@gmail.com>
2022-12-02 03:14:39 +0100

    feat: change hero section description

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
       <div className="text-center">
         <h1 className="text-4xl font-extrabold md:text-6xl">Sex'info</h1>
         <p className="mt-4 text-xl text-gray-600">
-          Le site qui vous aide à mieux comprendre le sexe.
+          Le serious game qui vous aide à mieux comprendre le sexe.
         </p>
 
         {/* Buttons */}
